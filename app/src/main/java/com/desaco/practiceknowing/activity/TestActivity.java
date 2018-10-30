9d15e9c2bbdf80ed22581e2b86fb81f138da6273
unknown <dengwen@netwin.com>
2018-10-30 16:17:32 +0800

    update

--- app/src/main/java/com/desaco/practiceknowing/activity/TestActivity.java
+++ app/src/main/java/com/desaco/practiceknowing/activity/TestActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.desaco.practiceknowing.R;
+
 /**
  * Created by desaco on 2018/10/18.
  */
@@ -13,6 +15,8 @@ public class TestActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //activity_test_layout  activity_layout
+        setContentView(R.layout.activity_test_layout);
 
     }
 
